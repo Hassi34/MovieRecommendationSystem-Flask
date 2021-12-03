acc292c9c0847c7356d283395357cb73fa89c5ed
Hasnain <hasnainmehmood3435@gmail.com>
2021-12-03 15:34:45 +0300

    New Movies

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'API_KEY';
+    var my_api_key = '404921924373365e328ce7dd5e96e9b7';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'API_KEY';
+  var my_api_key = '404921924373365e328ce7dd5e96e9b7';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
